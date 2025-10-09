640de1d581cdc9b86fa6301177b79569b3e737e8
Ashkan <AshkanHagh01@gmail.com>
2025-10-09 12:51:33 +0330

    chore(server): configure CORS settings

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { StoryExceptionFilter } from "./filter/exception-filter";
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix("/api/v1");
   app.useGlobalFilters(new StoryExceptionFilter());
+  app.enableCors(<CorsOptions>{
+    origin: process.env.CORS_ORIGIN || "*",
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
